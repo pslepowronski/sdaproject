8dbc68132120f6af724219de45c3e25942fcb5d5
pslepowronski <patryk.slepowronski@o2.pl>
2018-03-16 18:50:28 +0100

    initial commit

---
+++ src/main/java/com/sda/sdaproject/SdaprojectApplication.java
@@ -0,0 +1,12 @@
+package com.sda.sdaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sda/sdaproject/SdaprojectApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.sda.sdaproject;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SdaprojectApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
