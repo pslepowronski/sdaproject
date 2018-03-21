d754b77f236d0f2d3ef267813beb362a85467b1d
mihscko <m.mazurek85@gmail.com>
2018-03-21 20:47:48 +0100

    add + search Buyer
    (cherry picked from commit 70209e1)

--- src/main/java/com/sda/sdaproject/config/DatabaseConfig.java
+++ src/main/java/com/sda/sdaproject/config/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.sda.sdaproject.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
@@ -18,45 +19,9 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@EntityScan("com.sda.sdaproject")
 @EnableJpaRepositories(basePackages = "com.sda.sdaproject.repository")
 public class DatabaseConfig {
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.sda.sdaproject.entity");
-        em.setPersistenceUnitName("shop-pu");
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(properties);
-        return em;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop ");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("lokomotywA01P");
-        return dataSource;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-
-        return transactionManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
 }
-
