1e321e44bab9aeefa0f5fa1581fac1e906c2398d
mihscko <m.mazurek85@gmail.com>
2018-03-22 19:48:01 +0100

    addBuyer method

--- src/main/java/com/sda/sdaproject/config/BuyerService.java
+++ src/main/java/com/sda/sdaproject/config/BuyerService.java
@@ -1,4 +1,69 @@
 package com.sda.sdaproject.config;
 
+import com.sda.sdaproject.dto.BuyerCriteriaDto;
+import com.sda.sdaproject.dto.BuyerDto;
+import com.sda.sdaproject.entity.Buyer;
+import com.sda.sdaproject.entity.NewBuyerDto;
+import com.sda.sdaproject.repository.BuyerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
 public class BuyerService {
+
+    private final BuyerRepository buyerRepository;
+
+    @Autowired
+    public BuyerService(BuyerRepository buyerRepository) {
+        this.buyerRepository = buyerRepository;
+    }
+
+    public BuyerDto mapBuyer(Buyer b) {
+        return BuyerDto.builder()
+                .id(b.getId())
+                .firstName(b.getFirstName())
+                .lastName(b.getLastName())
+                .taxNumber(b.getTaxNumber())
+                .companyName(b.getCompanyName())
+                .street(b.getStreet())
+                .streetNumber(b.getStreetNumber())
+                .postcode(b.getPostcode())
+                .city(b.getCity())
+                .build();
+    }
+
+    public List<BuyerDto> findByAllCriteria(BuyerCriteriaDto c) {
+        return buyerRepository.findAll().stream().map(this::mapBuyer)
+                .filter(b -> b.getFirstName().contains(c.getFirstName()) || Objects.isNull(c.getFirstName()))
+                .filter(b -> b.getLastName().contains(c.getLastName()) || Objects.isNull(c.getLastName()))
+                .filter(b -> b.getCompanyName().contains(c.getCompanyName()) || Objects.isNull(c.getCompanyName()))
+                .filter(b -> b.getCity().contains(c.getCity()) || Objects.isNull(c.getCity()))
+                .collect(Collectors.toList());
+    }
+
+    public void deleteBuyerById(Integer id){
+        buyerRepository.delete(id);
+    }
+
+    public void addBuyer(NewBuyerDto buyer){
+        buyerRepository.save(getBuyerFromDto(buyer));
+    }
+
+    private Buyer getBuyerFromDto(NewBuyerDto buyer){
+        return Buyer.builder()
+                .firstName(buyer.getFirstName())
+                .lastName(buyer.getLastName())
+                .companyName(buyer.getCompanyName())
+                .taxNumber(buyer.getTaxNumber())
+                .street(buyer.getStreet())
+                .streetNumber(buyer.getStreetNumber())
+                .postcode(buyer.getPostcode())
+                .city(buyer.getCity())
+                .id(buyer.getId())
+                .build();
+    }
 }
---
+++ src/main/java/com/sda/sdaproject/entity/NewBuyerDto.java
@@ -0,0 +1,31 @@
+package com.sda.sdaproject.entity;
+
+import lombok.*;
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class NewBuyerDto {
+
+        private Integer id;
+
+        private String firstName;
+
+        private String lastName;
+
+        private String companyName;
+
+        private String taxNumber;
+
+        private String street;
+
+        private String streetNumber;
+
+        private String city;
+
+        private String postcode;
+    }
+
+
